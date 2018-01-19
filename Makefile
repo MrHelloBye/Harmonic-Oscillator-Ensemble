45090072650bf3cccceec425c7145400bfcda4ff
MrHelloBye <MrHelloBye@users.noreply.github.com>
2018-01-18 21:30:24 -0500

    Added uniform spacing test
    Started with uniform equilibrium spacing and zero momentum, and sure enough, I spotted an error.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 HO Ensemble: main.cpp vose.cpp vose.h
-	clang++ *.cpp -std=c++11 -o HO\ Ensemble.x
+	clang++ *.cpp -std=c++11 -o ho-ensemble.x
--- main.cpp
+++ main.cpp
@@ -15,51 +15,67 @@ double Hamiltonian(double*, double*, unsigned int);
 void update_state(double*, double*, unsigned int, double);
 void write_state(FILE*, double, double*, double*, unsigned int);
 
+double length = 10;
+double mass = 1e-6;
+
 int main(int argc, char *argv[])
 {
     auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
     mt19937 mt(seed); // Seeds Mersenne Twister with Device RNG
     
+    bool random_sampling = false;
     
     // Define the density array for position
     unsigned int grid_size = 10000; //number of grid points
-    
-    double *density = new double[grid_size];
-    double arg_scale = 2*PI/grid_size;
-    double norm = 0;
-    for (unsigned int i = 0; i<grid_size; i++)
-    {
-        density[i] = 0.5*sin(arg_scale*i)+1;
-        norm += density[i];
-    }
-    
-    //Normalize position density
-    for (unsigned int i = 0; i<grid_size; i++)
-    {
-        density[i] /= norm;
-    }
-    
-    // Sample locations
-    vose *sampler = new vose(density, grid_size, mt);
-    
-    double length = 10;
-    
     unsigned int particle_number = 10;
     
     double *positions = new double[grid_size];
-    for (unsigned int i=0; i<particle_number; i++)
-    {   
-        positions[i] = sampler->alias_method()*length/grid_size;
-    }
+    // Random sampling
+    if (random_sampling)
+    {
+        double *density = new double[grid_size];
+        double arg_scale = 2*PI/grid_size;
+        double norm = 0;
+        for (unsigned int i = 0; i<grid_size; i++)
+        {
+            density[i] = 0.5*sin(arg_scale*i)+1;
+            norm += density[i];
+        }
+    
+        //Normalize position density
+        for (unsigned int i = 0; i<grid_size; i++)
+        {
+            density[i] /= norm;
+        }
+    
+        // Sample locations
+        vose *pos_sampler = new vose(density, grid_size, mt);
+    
+        for (unsigned int i=0; i<particle_number; i++)
+        {   
+            positions[i] = pos_sampler->alias_method()*length/grid_size;
+        }
 
-    //Sort locations (just so IC is nicer)
-    sort(positions, positions+particle_number);
+        //Sort locations (just so IC is nicer)
+        sort(positions, positions+particle_number);
+    }
+    
+    // Even spacing
+    else
+    {
+        double spacing = length/(particle_number+1);
+        for (unsigned int i=0; i<particle_number; i++)
+        {
+            positions[i] = spacing*(i+1);
+        }
+    }
+    
     
     // Define momentum density
     double *mom_density = new double[grid_size];
-    double mass = 1.5, Temperature = 0.0009;
-    arg_scale = -1/(2*mass*Temperature);
-    norm = 0;
+    double Temperature = 1;
+    double arg_scale = -1/(2*mass*Temperature);
+    double norm = 0;
     for (unsigned int i = -floor(grid_size/2); i<floor(grid_size/2); i++)
     {
         mom_density[i] = exp(arg_scale*i*i);
@@ -72,13 +88,16 @@ int main(int argc, char *argv[])
         mom_density[i] /= norm;
     }
     
+    vose *mom_sampler = new vose(mom_density, grid_size, mt);
+    
     double *momenta = new double[grid_size];
     double mom_scale = 0.0005;
     unsigned int j;
     for (unsigned int i=0; i<particle_number; i++)
     {   
-        j = sampler->alias_method(); //momentum index
-        momenta[i] = (j-floor(grid_size/2))*mom_scale; //convert j to momentum
+        //j = mom_sampler->alias_method(); //momentum index
+        //momenta[i] = (j-float(grid_size)/2)*mom_scale; //convert j to momentum
+        momenta[i] = 0;
     }
     
     for (unsigned int i=0; i<particle_number; i++)
@@ -112,14 +131,13 @@ double potential(double *positions, unsigned int particle_number)
     double k = 0.1;
     double potential_energy = 0;
     double scale = k/2;
-    double length = 10;
     
     for (unsigned int i = 0; i<=particle_number; i++)
     {
         if (i==0)
             potential_energy += scale*pow(positions[0],2);
         else if (i==particle_number)
-            potential_energy += scale*pow(length-positions[i],2);
+            potential_energy += scale*pow(length-positions[i-1],2);
         else
             potential_energy += scale*pow(positions[i]-positions[i-1],2);
     }
